5d87fcfeb35e63102748f09f1f10129df6992529
druvieira <hertz996@gmail.com>
2026-05-20 12:37:10 -0400

    Redesign completo + PWA + login por filial + sessoes + identificacao

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'avioes-v1';
+const ASSETS = ['./avioes-app.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Sempre busca do servidor se online; cai no cache se offline
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
